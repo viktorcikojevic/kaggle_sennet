7215a4dc3447490201ad92dd8a1c110ba19a9ec1
viktorcikojevic <cikojevic.viktor@gmail.com>
2023-12-12 21:58:58 +0100

    cleaned constants

--- src/sennet/environments/constants.py
+++ src/sennet/environments/constants.py
@@ -5,7 +5,7 @@ from .environments import DATA_DUMPS_DIR
 # example code for environments.py
 # DATA_DUMPS_DIR = "/home/clay/research/kaggle/sennet/data_dumps"
 
-
+DATA_DUMPS_DIR = "/opt/kaggle/sennet/data_dumps"
 DATA_DUMPS_DIR = Path(DATA_DUMPS_DIR)
 PROCESSED_DATA_DIR = DATA_DUMPS_DIR / "processed"
 AUG_DUMP_DIR = DATA_DUMPS_DIR / "aug_dump"
