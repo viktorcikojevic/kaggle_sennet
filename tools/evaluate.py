6801674512a5fa3d469e8bbebb3f4fe6286e384a
sumo <sirapoab.c@gmail.com>
2023-12-25 11:36:34 +0000

    skip mask generation

--- tools/evaluate.py
+++ tools/evaluate.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 def main():
     # path = "/home/clay/research/kaggle/sennet/data_dumps/predicted/kidney_1_dense/submission.csv"
-    path = "/home/clay/research/kaggle/sennet/data_dumps/predicted/ensembled/kidney_3_dense/submission.csv"
+    # path = "/home/clay/research/kaggle/sennet/data_dumps/predicted/ensembled/kidney_3_dense/submission.csv"
+    path = "/home/clay/research/kaggle/sennet/data_dumps/predicted/ensembled/kidney_1_dense/submission.csv"
     print(f"{path = }")
     df = pd.read_csv(path)
     label = pd.read_csv(DATA_DIR / "train_rles.csv")
