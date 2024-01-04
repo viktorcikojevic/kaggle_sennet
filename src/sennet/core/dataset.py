2cd8684fcdb5325e3f227a82114ca7651235c714
sumo <sirapoab.c@gmail.com>
2024-01-04 22:49:37 +0000

    fix some training config crap + reverted normalisation order

--- src/sennet/core/dataset.py
+++ src/sennet/core/dataset.py
@@ -93,7 +93,7 @@ class ThreeDSegmentationDataset(Dataset):
         if self.augmenter is not None:
             self.transforms.append(self.augmenter)
         if self.normalisation_kwargs is not None:
-            self.transforms = [Normalise(**self.normalisation_kwargs)] + self.transforms
+            self.transforms.append(Normalise(**self.normalisation_kwargs))
 
     def __len__(self):
         return len(self.dataset)
